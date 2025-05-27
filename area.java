2a610af46e19f94c3d6cd695601a401a0950bc62
sapnasah <2022pcecssapna140@poornima.org>
2025-05-28 00:17:08 +0530

    add initial file

---
+++ area.java
@@ -0,0 +1,11 @@
+public static void main(String[] args) {
+    // Define the length and width of the rectangle
+    double length = 5.0;
+    double width = 3.0;
+
+    // Calculate the area of the rectangle
+    double area = length * width;
+
+    // Print the area to the console
+    System.out.println("The area of the rectangle is: " + area);
+}
